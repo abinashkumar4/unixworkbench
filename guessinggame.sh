00a0117157e6caf2de9d9444384ba90ffdd18e34
abinashkumar4 <33216571+abinashkumar4@users.noreply.github.com>
2018-11-07 03:55:53 -0700

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,29 +1,49 @@
 #!/usr/bin/env bash
-# File: guessinggame.sh
-
-dirarray=($(ls -d */))
-dircount=${#dirarray[@]}
-dirguess=0
-
-function checkguess {
-    # Usage of an if statement
-    if [[ $1 -lt $2 ]]
-    then
-    echo "echo Too low"
-    elif [[ $1 -gt $2 ]]
-    then
-    echo "echo Too high"
-    fi
+# File: guessinggame.sh.
+function compare_real_guess {
+	if [[ $2 =~ ^[0-9]+ ]]
+	then
+		if [[ $1 -eq $2 ]]
+		then
+			echo 0
+		elif [[  $1 -gt $2 ]]
+		then
+			echo 2
+		else
+			echo -1
+		fi
+	else
+		echo 9
+	fi
+}
+function hint_for_guess {
+	local text="Great guess!"
+	if [[ $1 -eq -1 ]]
+	then
+		text="the guess was too high... pick a lower number!"
+	elif [[ $1 -eq 2 ]]
+	then
+		text="the guess was too low... pick a higher number!"
+	elif [[ $1 -eq 9 ]]
+	then
+		text="the number of files must be an integer"
+	fi
+	echo $text
 }
 
-# Usage of a loop
-while [ $dirguess -ne $dircount ]
+files_in_dir=(*)
+numfiles=${#files_in_dir[*]}
+guessres=1
+
+echo "   Program guessing-game"
+echo "---------------------------"
+echo " How many files are there in the current directory?" 
+
+while [[ guessres -ne 0 ]]
 do
-echo "Guess the number of directories!"
-# Collecting user response
-read dirguess
-
-$(checkguess $dirguess $dircount)
-
+	echo "Please enter your guess and press Enter"
+	read numfguess
+	guessres=$(compare_real_guess $numfiles $numfguess)
+	text_instruct=$(hint_for_guess guessres)
+	echo $text_instruct
 done
-echo "You got it right!  Congrats!"
